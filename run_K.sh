dcd6613e52bcc90b3ed8b03c1751f52e3836b448
Daniel Sussman <dsussma3@jhu.edu>
2015-05-17 13:11:01 -0400

    First Try at clearing out git repo to a key core

--- run_K.sh
+++
@@ -1,1 +0,0 @@
-matlab -nodisplay -nosplash -r "simulate_K" > lsgm-sim-corr_9-K_2_4_500.output 2>&1
--- simulate_cores_paired.sh
+++
@@ -1,1 +0,0 @@
-matlab -nodisplay -nosplash -r "simulate_cores_paired" > simulate_cores_paired.output 2>&1
