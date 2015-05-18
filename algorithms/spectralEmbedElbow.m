3291744b971bd76201cda4d5159bcc8efb8ef1f8
Daniel Sussman <dsussma3@jhu.edu>
2015-05-18 09:53:07 -0400

    Cleaned up elbow finding spectral embed

--- algorithms/spectralEmbedElbow.m
+++ algorithms/spectralEmbedElbow.m
@@ -1,43 +1,48 @@
-function [XA, XB] = spectralEmbedElbow(A, B, numdim)
+function [XA, XB] = spectralEmbedElbow(A, B, numdim, projectToSphere)
 
-% embed A
+if nargin < 4
+    warning('projectToSphere not specified, using true as default');
+    projectToSphere = true;
+end
+
+%% Elbows for A
+
+% embed A to numDim+2 dimension
 [XA, svalA, ~] = svds(A, min([numdim+2, length(A)]) );
-% find the "elbow" in the curve
-d1 = diff(diag(svalA));
-d2_A = diff(d1)';
-[~, dim]  = max(d2_A);
-% find second elbow
-d2_A(dim) = 0;
-[~, dim2] = max(d2_A(dim:end));
-%% find third elbow
-%d2_A(dim2) = 0;
-%[~, dim3] = max(d2_A(dim+dim2-1:end));
-dimA = dim+dim2;
-%dimA = dim+1;
+
+% find first and second derivatives
+d1_A = diff(diag(svalA));
+d2_A = diff(d1_A)';
+
+% find the first "elbow" in the curve via max of second differences
+[~, dimA]  = max(d2_A);
+% find the second elbow as the next highest after the first
+d2_A(1:dimA) = 0;
+[~, dimA] = max(d2_A);
+
+%% Elbows for B
 
 % embed B
 [XB, svalB, ~] = svds(B, min([numdim+2, length(B)]) );
 % find the "elbow" in the curve
-d1 = diff(diag(svalB));
-d2_B = diff(d1)';
-[~, dim] = max(d2_B);
+d1_B = diff(diag(svalB));
+d2_B = diff(d1_B)';
+[~, dimB] = max(d2_B);
 % find second elbow
-d2_B(dim) = 0;
-[~, dim2] = max(d2_B(dim:end));
-%% find second elbow
-%d2_B(dim2) = 0;
-%[~, dim3] = max(d2_B(dim+dim2-1:end));
-dimB = dim+dim2;
-%dimB = dim+1;
+d2_B(1:dimB) = 0;
+[~, dimB] = max(d2_B(dimB:end));
 
-% use max of 2 elbows
+%% use max of 2 elbows
 dim = max(dimA, dimB);
 
 
-% Embed both
+%% Scale the eigenvectors to get the embedding Embed both
 XA = XA(:,1:dim)*sqrt(svalA(1:dim,1:dim));
 XB = XB(:,1:dim)*sqrt(svalB(1:dim,1:dim));
 
-% project to sphere
-XA = XA *diag(sum(XA.^2).^(-.5));
-XB = XB *diag(sum(XB.^2).^(-.5));
+if(projectToSphere)
+    XA = XA *diag(sum(XA.^2).^(-.5));
+    XB = XB *diag(sum(XB.^2).^(-.5));
+end
+
+end
