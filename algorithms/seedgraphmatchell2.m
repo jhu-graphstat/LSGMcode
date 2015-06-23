cd4cd64b8f7ae28f5f989bf1657d38ad7724f12a
Daniel Sussman <dsussma3@jhu.edu>
2015-06-23 10:22:42 -0400

    See #11. Now uses full matrix and uses the doubly stochastic matrix that is returned.

--- algorithms/seedgraphmatchell2.m
+++ algorithms/seedgraphmatchell2.m
@@ -1,4 +1,4 @@
-function [ corr, corr_c ] = seedgraphmatchell2(A,B,seeds,start)% ,alpha_type )
+function [ corr, corr_c ] = seedgraphmatchell2(A,B,seeds,start)
 % Returns matching for the SGM problem using the SGM algorithm
 % 
 % [corr,corr_c] = seedgraphmatchell2(A,B,m,start)
@@ -56,16 +56,15 @@ if( strcmp(start,'bari' ))
     % Use the baricenter
 	P = ones(n)/n;
 elseif( strcmp(start,'convex'))
-    % fprintf('Start Conv Relax\n')
     % use the start from the convex relaxation
-	[~,P]=relaxed_normAPPB_FW_seeds(A22,B22,s);
-    % fprintf('Done Conv Relax\n')
+	P=relaxed_normAPPB_FW_seeds(A,B,seeds);
+    P = P(nonSeeds,nonSeeds);
 else
     P = start;
 end
 
 %% Matching just using seed to non-seed information
-if s > 0
+if s > 0 && nargout > 1
     Ptotv = zeros(totv);
     Ptotv(nonSeeds,nonSeeds) = A21*B21'+A12'*B12;
     Ptotv(seeds,seeds) = eye(numel(seeds));
@@ -86,34 +85,29 @@ while (toggle==1)&&(iter<patience)
     ind = lapjv( -Grad, scale );%YiCaoHungarian(-Grad);%
     T=eyen(ind,:);
     
-%    if (alpha_type == 2)
-%	    alpha = 2/(2+iter);
-%	    P = (1-alpha)*P+(alpha)*T;
-%    else
+    % Compute some temporary quantities
+    c=trace(A22'*P*B22*P');
+    d=trace(A22'*T*B22*P')+trace(A22'*P*B22*T');
+    e=trace(A22'*T*B22*T');
+    u=trace(P'*A21*B21'+P'*A12'*B12);
+    v=trace(T'*A21*B21'+T'*A12'*B12);
 
-        % Compute some temporary quantities
-		c=trace(A22'*P*B22*P');
-		d=trace(A22'*T*B22*P')+trace(A22'*P*B22*T');
-		e=trace(A22'*T*B22*T');
-		u=trace(P'*A21*B21'+P'*A12'*B12);
-		v=trace(T'*A21*B21'+T'*A12'*B12);
-        
-        % Determine step size
-		alpha=-(d-2*e+u-v)/(2*(c-d+e));
-		f0=0;
-		f1=c-e+u-v;
-		falpha=(c-d+e)*alpha^2+(d-2*e+u-v)*alpha;
-        
-        % Take the right step
-		if (alpha<tol)&&(alpha>0)&&(falpha>f0)&&(falpha>f1)
-		    P=alpha*P+(1-alpha)*T;
-		elseif (f0>f1)
-		    P=T;
-        else
-            % Stop now
-		    toggle=0;
-		end
-%	end
+    % Determine step size
+    alpha=-(d-2*e+u-v)/(2*(c-d+e));
+    f0=0;
+    f1=c-e+u-v;
+    falpha=(c-d+e)*alpha^2+(d-2*e+u-v)*alpha;
+
+    % Take the right step
+    if (alpha<tol)&&(alpha>0)&&(falpha>f0)&&(falpha>f1)
+        P=alpha*P+(1-alpha)*T;
+    elseif (f0>f1)
+        P=T;
+    else
+        % Stop now
+        toggle=0;
+    end
+
 end
 
 %% Get the final correspondence
@@ -121,10 +115,4 @@ Ptotv = zeros(totv);
 Ptotv(nonSeeds,nonSeeds) = P;
 Ptotv(seeds,seeds) = eye(numel(seeds));
 corr = lapjv(-Ptotv, scale);%YiCaoHungarian(-P);%
-
-
-% %init_vs_final_dissagreements = sum(ind0~=corr)
-% corr = 1:totV;
-% corr(seeds) = seeds;
-% corr(nonSeeds) = corrNS;
 
