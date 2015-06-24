2beec3a691eeb5ec191139279ed8c0a00b1ea5dc
Jason <jtmatterer@gmail.com>
2015-06-24 12:33:36 -0400

    Added diagonal adjustment of degree/(num nodes) to the regularized laplacian code in algorithms/regularizedLaplacian.m

--- algorithms/regularizedLaplacian.m
+++ algorithms/regularizedLaplacian.m
@@ -12,7 +12,8 @@ nrows = length(rowSums);
 % round off errors for large graphs
 regularizer = sum(rowSums/nrows);
 D_sqrt = sparse(1:nrows, 1:nrows, 1./sqrt(rowSums + regularizer)); % Sparse diagonal matrix
+diagonal_adj = sparse(1:nrows, 1:nrows, rowSums/(nrows-1));
 
-L = D_sqrt * A * D_sqrt;
+L = D_sqrt * (A - diagonal_adj) * D_sqrt;
 
 end
