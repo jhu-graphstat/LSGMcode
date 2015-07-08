27c40d8cb6e6a613c7fc8a924afec09143391b53
Jason <jtmatterer@gmail.com>
2015-07-08 10:17:34 -0400

    algorithms/addDummyNodes.m : added function to add dummy nodes to adjacency matrices

---
+++ algorithms/addDummyNodes.m
@@ -0,0 +1,27 @@
+function [A, B] = addDummyNodes(pieceA, pieceB)
+
+    nPieceA = length(pieceA);
+    nPieceB = length(pieceB);
+    
+    % If the number of vertices from each graph are not equal in the
+    % cluster, add dummy vertices for the smaller graph
+    if nPieceA < nPieceB
+        % To handle matching dummy vertices, we turn true nonedges in the
+        % adjacency matrices into -1's
+        A = 2*pieceA - ones(nPieceA);
+        B = 2*pieceB - ones(nPieceB);
+        diff = nPieceB - nPieceA;
+        Z12 = zeros(nPieceA, diff);
+        Z22 = zeros(diff);
+        A = [A, Z12; Z12', Z22]; % dummy vertices are disconnected
+    elseif nPieceA > nPieceB
+        % To handle matching dummy vertices, we turn true nonedges in the
+        % adjacency matrices into -1's
+        A = 2*pieceA - ones(nPieceA);
+        B = 2*pieceB - ones(nPieceB);
+        diff = nPieceA - nPieceB;
+        Z12 = zeros(nPieceB, diff);
+        Z22 = zeros(diff);
+        B = [B, Z12; Z12', Z22]; % dummy vertices are disconnected
+    end
+end
